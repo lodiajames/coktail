311494a3d6fd5a305ef78eb3752e729a86c5c944
Ibrahima diallo <lodiajames@hotmail.com>
2021-11-05 12:50:29 -0400

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return <section className="section about-section">
+      <h1 className='section-titles'>about us</h1>
+        <p>Flexitarian deep v cronut lorem you probably haven't heard of them bitters taiyaki helvetica VHS readymade pitchfork labore migas single-origin coffee photo booth. Cronut sed lomo affogato, ea yuccie laborum literally shoreditch PBR&B deserunt. Mollit chambray veniam narwhal iceland celiac selfies 90's echo park asymmetrical 
+          lumbersexual gentrify seitan try-hard vexillologist. Kinfolk locavore cornhole bicycle rights kitsch jean shorts
+        </p>
+  </section>
+}
